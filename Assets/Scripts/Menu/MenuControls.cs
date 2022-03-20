e99fe60294a957e01b19d67839d32ae220879018
David Maciejewski <david_maciejewski@web.de>
2022-03-20 15:12:40 +0100

    Added rules menu

--- Assets/Scripts/Menu/MenuControls.cs
+++ Assets/Scripts/Menu/MenuControls.cs
@@ -11,14 +11,40 @@ public class MenuControls : MonoBehaviour
     public GameObject MainMenu;
     public GameObject OptionsMenu;
     public GameObject PlayMenu;
+    public GameObject ControlsMenu;
+    public GameObject RulesMenu;
 
     public Dropdown ThemeDropdown;
     public Dropdown CardsPerPlayerDropdown;
 
+    public Text RuleText;
+
     private Options _options;
+
+    private List<string> RuleTexts;
+    private int CurrentText;
 
     private void Start()
     {
+        RuleTexts = new List<string>()
+        {
+            @"Search the Labyrinth for your magical objects by carefully moving through the constantly moving maze.
+
+            The first player to find all of their objects and then return to the starting square is the winner.",
+            @"On your turn, look at your card by clicking on it without showing it to the other players. You now have to try and get to the square showing the same picture as on your card. To do this, first insert a maze tile and then move your playing piece.",
+            @"Along the edge of the board are 12 arrows. When it is your turn you must choose one of the arrows and then insert a maze tile, which will push out a maze tile at the opposite end.
+
+            The tile that is pushed out remains on the edge of the board until it is inserted elsewhere during the next player's turn.
+
+            A maze tile cannot be pushed back at the same place as the previous player pushed it out.",
+            @"After shifting the maze, move your playing character in first person as far as you like along the open pathway.
+
+            If you are unable to reach your goal directly get into the best possible starting position for your next turn. However, moving is not compulsory so the character also may be left where it is. You do not have to move if you don't want to.",
+            @"The first player to find all of his collectables and get his character back onto its starting position is the winner."
+        };
+
+        CurrentText = 0;
+
         _options = new Options();
         _options.Load();
         _options.SetGlobals();
@@ -50,6 +76,8 @@ public class MenuControls : MonoBehaviour
         MainMenu.SetActive(true);
         OptionsMenu.SetActive(false);
         PlayMenu.SetActive(false);
+        ControlsMenu.SetActive(false);
+        RulesMenu.SetActive(false);
     }
 
     public void ShowOptionsMenu()
@@ -57,6 +85,8 @@ public class MenuControls : MonoBehaviour
         MainMenu.SetActive(false);
         OptionsMenu.SetActive(true);
         PlayMenu.SetActive(false);
+        ControlsMenu.SetActive(false);
+        RulesMenu.SetActive(false);
     }
 
     public void ShowPlayMenu()
@@ -64,6 +94,42 @@ public class MenuControls : MonoBehaviour
         MainMenu.SetActive(false);
         OptionsMenu.SetActive(false);
         PlayMenu.SetActive(true);
+        ControlsMenu.SetActive(false);
+        RulesMenu.SetActive(false);
+    }
+
+    public void ShowControlsMenu()
+    {
+        MainMenu.SetActive(false);
+        OptionsMenu.SetActive(false);
+        PlayMenu.SetActive(false);
+        ControlsMenu.SetActive(true);
+        RulesMenu.SetActive(false);
+    }
+
+    public void ShowRulesMenu()
+    {
+        MainMenu.SetActive(false);
+        OptionsMenu.SetActive(false);
+        PlayMenu.SetActive(false);
+        ControlsMenu.SetActive(false);
+        RulesMenu.SetActive(true);
+    }
+
+    public void RulesNextText()
+    {
+        if (CurrentText < RuleTexts.Count - 1)
+            CurrentText++;
+
+        RuleText.text = RuleTexts[CurrentText];
+    }
+
+    public void RulesPreviousText()
+    {
+        if (CurrentText > 0)
+            CurrentText--;
+
+        RuleText.text = RuleTexts[CurrentText];
     }
 
     public void ChangeTheme(int value)
